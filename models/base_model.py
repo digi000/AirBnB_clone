9547170bb0d240d6bc7f0f345a5bcf9f588c45b7
Tuyisenge Jordan <jordanale.112@gmail.com>
2024-03-10 13:55:54 -0700

    task 3 update

--- models/base_model.py
+++ models/base_model.py
@@ -7,15 +7,21 @@ class BaseModel:
         self.created_at = created_at
         self.updated_at = updated_at
         self.id = str(uuid.uuid4())
-        self.created_at = str(datetime.datetime.now().isoformat())
-        self.updated_at = str(datetime.datetime.now().isoformat())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        self.updated_at = str(datetime.datetime.now().isoformat())
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        self.__dict__["__class__"] = __class__.__name__
-        return self.__dict__
+        return {
+                "my_number":self.my_number,
+                "name":self.name,
+                "__class__":__class__.__name__,
+                "updated_at":self.updated_at.isoformat(),
+                "id":self.id,
+                "created_at":self.created_at.isoformat()
+                }
