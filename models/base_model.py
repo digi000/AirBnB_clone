171bce7adf76ef6bc003ddee0c00edd9453b20fb
Tuyisenge Jordan <jordanale.112@gmail.com>
2024-03-12 05:48:46 -0700

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -1,27 +1,32 @@
 import uuid
-import datetime
+from datetime import datetime
+
 
 class BaseModel:
-    def __init__(self, id=None, created_at=None, updated_at=None):
-        self.id = id
-        self.created_at = created_at
-        self.updated_at = updated_at
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+    def __init__(self, *args, **kwargs):  
+        if kwargs:    
+            for key,value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at','updated_at'):
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         return {
-                "my_number":self.my_number,
-                "name":self.name,
-                "__class__":__class__.__name__,
-                "updated_at":self.updated_at.isoformat(),
-                "id":self.id,
-                "created_at":self.created_at.isoformat()
+                "my_number": self.my_number,
+                "name": self.name,
+                "__class__": __class__.__name__,
+                "updated_at": self.updated_at.isoformat(),
+                "id": self.id,
+                "created_at": self.created_at.isoformat()
                 }
