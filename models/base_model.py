e8a47ad34ffbe5ab79235b1f4afac370ceedfd86
Tuyisenge Jordan <jordanale.112@gmail.com>
2024-03-14 09:53:16 -0700

    task 4f

--- models/base_model.py
+++ models/base_model.py
@@ -3,10 +3,18 @@ import datetime
 
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for ky, vl in kwargs.items():
+                if ky != "__class__":
+                    if ky == 'created_at' or ky == 'updated_at':
+                        df = '%Y-%m-%dT%H:%M:%S.%f'
+                        vl = datetime.datetime.strptime(vl, df)
+                    setattr(self, ky, vl)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
