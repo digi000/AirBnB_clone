503e84118a949638b5aee1a7ab92b6222f9c6d36
Tuyisenge Jordan <jordanale.112@gmail.com>
2024-03-14 07:11:36 -0700

    task 3f

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,1 @@
-from .engine.file_storage import FileStorage
 
-storage = FileStorage()
-storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,21 +1,21 @@
 import uuid
-from datetime import datetime
+import datetime
+
 
 class BaseModel:
     def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+       self.id = str(uuid.uuid4())
+       self.created_at = datetime.datetime.now()
+       self.updated_at = datetime.datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        dict_copy = self.__dict__.copy()
-        dict_copy["__class__"] = self.__class__.__name__
-        dict_copy["created_at"] = dict_copy["created_at"].isoformat()
-        dict_copy["updated_at"] = dict_copy["updated_at"].isoformat()
-        return dict_copy
+        self.__dict__['__class__'] = __class__.__name__
+        self.__dict__['created_at'] = self.created_at.isoformat()
+        self.__dict__['updated_at'] = self.updated_at.isoformat()
+        return self.__dict__
