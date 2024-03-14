ca2664ab7fd3ba15a59ea6473dd9490121d547cf
Tuyisenge Jordan <jordanale.112@gmail.com>
2024-03-14 07:18:39 -0700

    pycs task3f

--- models/base_model.py
+++ models/base_model.py
@@ -4,9 +4,9 @@ import datetime
 
 class BaseModel:
     def __init__(self):
-       self.id = str(uuid.uuid4())
-       self.created_at = datetime.datetime.now()
-       self.updated_at = datetime.datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
