fbb6a76c4c24ba7bbe7f379cca84787904f79748
Tuyisenge Jordan <jordanale.112@gmail.com>
2024-03-17 00:00:07 +0200

    task 6

---
+++ console.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb)'
+
+    def emptyline(self):
+            pass
+    def do_quit(self, args):
+        '''Quit command to exit the program'''
+        return True
+    
+    def do_EOF(self, args):
+        '''Exit the program when EOF (Ctrl+D) is reached'''
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
