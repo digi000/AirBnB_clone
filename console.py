f393f5501620dc8e928a81b324ed46dda7f55dd2
Tuyisenge Jordan <jordanale.112@gmail.com>
2024-03-17 00:13:02 +0200

    task6 f .1

--- console.py
+++ console.py
@@ -8,12 +8,13 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         pass
 
-    def do_quit(self, args):
+    def do_quit(self, arg):
         '''Quit command to exit the program'''
         return True
     
-    def do_EOF(self, args):
+    def do_EOF(self, arg):
         '''Exit the program when EOF (Ctrl+D) is reached'''
+        print()
         return True
 
 
