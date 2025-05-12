76034fc22119b978943449199e03751c9e25fda6
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-05-12 23:28:55 +0200

    4t

--- models/base_model.py
+++ models/base_model.py
@@ -4,11 +4,18 @@ import uuid
 from datetime import datetime
 class BaseModel:
     """3. BaseModel"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """intializer"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            self.id = kwargs["id"]
+            self.my_number = kwargs["my_number"]
+            self.name = kwargs["name"]
+            self.created_at = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """string representation"""
